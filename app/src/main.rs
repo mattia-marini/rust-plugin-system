855ccc3d8b9b9df81779d072fe087f37df74c582
Mattia Marini <marmattia26@gmail.com>
2025-03-19 23:27:21 +0100

    Code cleanup

--- app/src/main.rs
+++ app/src/main.rs
@@ -18,21 +18,4 @@ pub fn load_and_run_plugin(plugin_name: &'static str) {
 fn main() {
     load_and_run_plugin("plugin1");
     load_and_run_plugin("plugin2");
-    //unsafe {
-    //    // Loading plguin 1
-    //
-    //
-    //    //let lib = Library::new("./target/debug/libplugin1.dylib").expect("Failed to load plugin");
-    //    //let plugin_1_func: Symbol<fn(*const apis::ApisData)> =
-    //    //    lib.get(b"init_apis").expect("Failed to get function");
-    //    //
-    //    //// Loading plguin 2
-    //    //let lib = Library::new("./target/debug/libplugin2.dylib").expect("Failed to load plugin");
-    //    //let plugin_2_func: Symbol<fn(*const apis::ApisData)> =
-    //    //    lib.get(b"init_apis").expect("Failed to get function");
-    //    //
-    //    //// println!("apis in app {:?}", apis_impl::APIS.val);
-    //    //plugin_1_func(&apis_impl::APIS);
-    //    //plugin_2_func(&apis_impl::APIS);
-    //}
 }
--- plugin1/src/lib.rs
+++ plugin1/src/lib.rs
@@ -10,8 +10,3 @@ pub extern "C" fn main() {
     apis::api1();
     apis::api2();
 }
-
-//#[link(name = "my_plugin")]
-//extern "C" {
-//    fn add(a: u32, b: u32) -> u32;
-//}
